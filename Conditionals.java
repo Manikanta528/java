548811953b614db71a644b98c01d489024d4defd
Manikanta528 <chmanikanta528@gmail.com>
2021-12-05 20:40:20 +0530

    Conditionals and loops

---
+++ Conditionals.java
@@ -0,0 +1,27 @@
+package com.manikanta;
+
+public class ConditionalsAndLoops {
+    public static void main(String[] args) {
+        /*
+        Multiline
+        comments
+        */
+        int sal = 55450;
+//        if(sal > 25000){
+//            sal = sal + 5000;
+//        }else {
+//            sal = sal + 2500;
+//        }
+
+        // multiple if-else
+
+        if( sal > 60000){
+            sal += 6000;
+        }else if ( sal > 50000){
+            sal += 5000;
+        }else{
+            sal += 2500;
+        }
+        System.out.println(sal);
+    }
+}
---
+++ Loops.java
@@ -0,0 +1,2 @@
+package com.manikanta;public class Loops {
+}
