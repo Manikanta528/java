b6a69f4657147fbe30b9358dc2e6eda27d259e0c
Manikanta528 <chmanikanta528@gmail.com>
2021-12-22 22:16:21 +0530

    linear Search example

---
+++ FindMin.java
@@ -0,0 +1,29 @@
+package com.manikanta;
+
+public class FindMin {
+    public static void main(String[] args) {
+        int[] arr = {18, 19, -3, 2, -2744, -31, 17};
+        System.out.println(min(arr));
+        System.out.println(max(arr));
+    }
+
+    static int max(int[] arr) {
+        int maxnum =arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(maxnum < arr[i]){
+                maxnum = arr[i];
+            }
+        }
+        return maxnum;
+    }
+
+    static int min(int[] arr) {
+        int minnum = arr[0];
+         for (int i = 1; i < arr.length; i++) {
+             if(minnum > arr[i]){
+                 minnum = arr[i];
+             }
+         }
+         return minnum;
+    }
+}
