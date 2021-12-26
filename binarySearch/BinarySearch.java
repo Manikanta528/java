0d6ee9c036f043c3149f6709b1eddcb5897350f8
Manikanta528 <chmanikanta528@gmail.com>
2021-12-26 09:39:50 +0530

    Binary Search intro

---
+++ binarySearch/BinarySearch.java
@@ -0,0 +1,38 @@
+package com.manikanta.binarySearch;
+
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -11, -9, -2,-1, 0 , 4, 12, 22, 44, 83,100};
+        int target = -11;
+        int ans = binarySearch(arr , target);
+        System.out.println(ans);
+    }
+    //return the index of target else return -1 if don't exist
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end ){
+
+            int mid = start + (end - start )/2;
+            if ( arr[start] == target ){
+                return start;
+            }
+            if ( arr[end] == target){
+                return end;
+            }
+            if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            else if(target > arr[mid]) {
+                start = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+       return -1;
+    }
+}
---
+++ binarySearch/OrderAgnosticBS.java
@@ -0,0 +1,58 @@
+package com.manikanta.binarySearch;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        //int[] arr = {-18, -11, -9, -2,-1, 0 , 4, 12, 22, 44, 83,100};
+        int[] arr ={100 , 99, 87, 74, 57, 44,32, 21, 12, 3, -4};
+        int target = 3;
+        int ans = orderAgnosticBS(arr, target);
+        System.out.println(ans);
+    }
+    static int orderAgnosticBS(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        boolean iaOA = arr[start] < arr[end];
+        if(iaOA == true) {
+            while (start < end) {
+
+                int mid = start + (end - start) / 2;
+                if (arr[start] == target) {
+                    return start;
+                }
+                if (arr[end] == target) {
+                    return end;
+                }
+                if (arr[mid] == target) {
+                    return mid;
+                } else if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                }
+            }
+        }
+        else{
+                while (start < end ){
+
+                    int mid = start + (end - start )/2;
+                    if ( arr[start] == target ){
+                        return start;
+                    }
+                    if ( arr[end] == target){
+                        return end;
+                    }
+                    if(arr[mid] == target) {
+                        return mid;
+                    }
+                    else if (target > arr[mid]) {
+                        end = mid - 1;
+                    }
+                    else if(target < arr[mid]) {
+                        start = mid + 1;
+                    }
+
+                }
+        }
+    return -1;
+}
+}
