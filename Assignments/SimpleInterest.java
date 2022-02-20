0c764503bff891f3f81233efd6735814eb80ba8c
Manikanta528 <chmanikanta528@gmail.com>
2022-02-20 12:34:52 +0530

    02 first java

---
+++ Assignments/SimpleInterest.java
@@ -0,0 +1,19 @@
+package com.manikanta.Assignments;
+import java.util.*;
+
+// Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+// https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/assignments/02-first-java.md
+
+public class SimpleInterest {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principal amount :");
+        int principal = sc.nextInt();
+        System.out.print("Enter time ( in years ):");
+        int time = sc.nextInt();
+        System.out.print("Enter annual interest rate :");
+        int rate = sc.nextInt();
+        int si = principal + (1 + rate * time);
+        System.out.println("Simple Interest : "+ si);
+    }
+}
