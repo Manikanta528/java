a62e6ea9c41324c557fb7b3c43864318e164dff7
Manikanta528 <chmanikanta528@gmail.com>
2022-04-07 05:03:26 +0530

    commented

--- binarySearch/GuessNumberHigherOrLower.java
+++ binarySearch/GuessNumberHigherOrLower.java
@@ -10,23 +10,23 @@ package com.manikanta.binarySearch;
  * int guess(int num);
  */
 
-public class GuessNumberHigherOrLower {
-    public static void main(String[] args) {
-
-    }
-    static int guessNumber(int n){
-        int start = 1, end = n;
-        while(start <= end ){
-            int mid = start + (end -start) / 2;
-            int guess = guess(mid);
-            if(guess == 0) return mid;
-            else if (guess == 1 ){//num < pick
-                start = end -1;
-            }
-            else{//num > pick
-                end = start + 1;
-            }
-        }
-        return -1;
-    }
-}
+//public class GuessNumberHigherOrLower {
+//    public static void main(String[] args) {
+//
+//    }
+//    static int guessNumber(int n){
+//        int start = 1, end = n;
+//        while(start <= end ){
+//            int mid = start + (end -start) / 2;
+//            int guess = guess(mid);
+//            if(guess == 0) return mid;
+//            else if (guess == 1 ){//num < pick
+//                start = end -1;
+//            }
+//            else{//num > pick
+//                end = start + 1;
+//            }
+//        }
+//        return -1;
+//    }
+//}
