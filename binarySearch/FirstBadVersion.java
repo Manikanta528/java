139587da18a46e4d7621531b90be22dadeafff06
Manikanta528 <chmanikanta528@gmail.com>
2022-04-07 05:05:33 +0530

    leetcode Q.278

---
+++ binarySearch/FirstBadVersion.java
@@ -0,0 +1,24 @@
+package com.manikanta.binarySearch;
+
+//public class FirstBadVersion {
+//    public static void main(String[] args){
+//
+//    }
+//    static int firstBadVersion(int n){
+//        int start = 1, end = n;
+//        while(start <= end){
+//            int mid = start + (end- start) /2;
+//            boolean badV = isBadVersion(mid);
+//            boolean badPreV = isBadVersion(mid-1);
+//            if(badV == true && badPreV == false) return mid;
+//            else if(badV == false){
+//                start = mid + 1;
+//            }
+//            else{
+//                end = mid -1;
+//            }
+//
+//        }
+//        return -1;
+//    }
+//}
