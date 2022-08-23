162257222d25c9f01cbebc4d0a29efd6a66e08e2
Manikanta528 <chmanikanta528@gmail.com>
2022-08-23 21:54:04 +0530

    Packages intro

---
+++ oop/packages/a/Greeting.java
@@ -0,0 +1,11 @@
+package com.manikanta.oop.packages.a;
+
+import static com.manikanta.oop.packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        message();
+    }
+
+}
