38c08dfce2956c80ed0e0521c3c4ec854cca92b3
Manikanta528 <chmanikanta528@gmail.com>
2022-09-02 15:51:38 +0530

    interface extends demo

---
+++ oop/interfaces/extendDemo/A.java
@@ -0,0 +1,5 @@
+package com.manikanta.oop.interfaces.extendDemo;
+
+public interface A {
+    void fun();
+}
---
+++ oop/interfaces/extendDemo/B.java
@@ -0,0 +1,5 @@
+package com.manikanta.oop.interfaces.extendDemo;
+
+public interface B extends A{
+    void greet();
+}
