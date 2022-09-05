73dcf781dcd70936ef54d8b5c2d9a1354097edb2
Manikanta528 <chmanikanta528@gmail.com>
2022-09-05 20:12:56 +0530

    Collections framework & vectors

---
+++ oop/collections/Main.java
@@ -0,0 +1,27 @@
+package com.manikanta.oop.collections;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+//        list2.add(27);
+//        list2.add(28);
+//        list2.add(29);
+//        list2.add(30);
+//        list2.add(31);
+//
+//        System.out.println(list2);
+
+        List<Integer> list3 = new Vector<>();
+
+        list3.add(01);
+        list3.add(02);
+        list3.add(03);
+        list3.add(05);
+        System.out.println(list3);
+
+    }
+}
