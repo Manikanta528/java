756ae9a58c7a4416792e1f463f81d03f0a3569c3
Manikanta528 <chmanikanta528@gmail.com>
2022-09-08 22:11:09 +0530

    Doubly Linked List

--- linkedList/Main.java
+++ linkedList/Main.java
@@ -2,20 +2,30 @@ package com.manikanta.linkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(11);
+//        list.insertFirst(24);
+//        list.insertFirst(36);
+//        list.insertLast(2);
+//        list.insert(27,3);
+//        System.out.println(list.find(24));
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(2));
+//        list.display();
+
+        DLL list = new DLL();
         list.insertFirst(3);
         list.insertFirst(11);
         list.insertFirst(24);
         list.insertFirst(36);
-        list.insertLast(2);
-        list.insert(27,3);
-        System.out.println(list.find(24));
-        list.display();
-        System.out.println(list.deleteFirst());
-        list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println(list.delete(2));
+        list.insertLast(43);
+        list.insertLast(50);
+        list.insert(43, 44);
         list.display();
     }
 }
